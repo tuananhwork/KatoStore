34e5b0246e984f9f4e93633d6f976dea5dc82d20
Tuan Anh <chubatuananh.work@gmail.com>
2025-09-24 09:09:18 +0700

    fix otp render

--- server/src/config/mailer.js
+++ server/src/config/mailer.js
@@ -2,8 +2,8 @@ const nodemailer = require('nodemailer');
 
 const transporter = nodemailer.createTransport({
   host: process.env.SMTP_HOST,
-  port: Number(process.env.SMTP_PORT || 587),
-  secure: process.env.SMTP_SECURE === 'true',
+  port: process.env.SMTP_PORT,
+  secure: false, // vì dùng port 587
   auth: {
     user: process.env.SMTP_USER,
     pass: process.env.SMTP_PASS,
